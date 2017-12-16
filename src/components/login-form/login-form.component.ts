5150fa7bdd04591765981eec1cacd7c5cae7db3b
Swagat Swain <ssswagatss@gmail.com>
2017-12-16 22:43:32 +0530

    Implements login

--- src/components/login-form/login-form.component.ts
+++ src/components/login-form/login-form.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular/navigation/nav-controller';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { ToastController } from 'ionic-angular/components/toast/toast-controller';
+import { Account } from '../../models/account/account.interface'
 
 @Component({
   selector: 'app-login-form',
@@ -7,12 +10,35 @@ import { NavController } from 'ionic-angular/navigation/nav-controller';
 })
 export class LoginFormComponent {
 
+  account={} as Account;
 
-  constructor(private navCtrl: NavController) {
+  constructor(private navCtrl: NavController, 
+    private afAuth : AngularFireAuth, private toast:ToastController) {
     console.log('Hello LoginFormComponent Component');
   }
 
   navigateToPage(page: string) {
     page === 'TabsPage' ? this.navCtrl.setRoot('TabsPage') : this.navCtrl.push(page);
   }
+  async login() {
+    try{
+     const result = await this.afAuth
+     .auth.signInWithEmailAndPassword(this.account.email,this.account.password)
+ 
+     this.toast.create({
+       message:'Logged in successfully.',
+       duration:3000,
+       position:'top'
+     }).present();
+ 
+     console.log(result);
+    }catch(e){
+      console.log('error',e);
+      this.toast.create({
+        message:e.message,
+        duration:3000,
+        position:'top'
+      }).present();
+    }
+   }
 }
--- src/pages/login/login.module.ts
+++ src/pages/login/login.module.ts
@@ -2,6 +2,7 @@ import { ComponentsModule } from './../../components/components.module';
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { LoginPage } from './login';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
 @NgModule({
   declarations: [
@@ -9,7 +10,8 @@ import { LoginPage } from './login';
   ],
   imports: [
     IonicPageModule.forChild(LoginPage),
-    ComponentsModule
+    ComponentsModule,
+    AngularFireAuthModule
   ],
 })
 export class LoginPageModule { }
